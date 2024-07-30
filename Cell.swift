74002b3e751113d74a94465f52dc46ce8343d205
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-30 11:27:50 +0300

    ADD: VStack padding

--- Cell.swift
+++ Cell.swift
@@ -12,9 +12,8 @@ struct Cell: View {
         VStack(alignment: .leading) {
             TopView()
             Text("Do you want to download free song for ipod? If so, reading this article could save you from getting in to a lot of trouble! Downloading music to your Ipod has more than one pitfall associated with it, and this article will tell you the best way to download free song for Ipod.")
-            .lineLimit(nil)
-            .padding()
-        }
+                .lineLimit(nil)
+        }.padding()
     }
 }
 
