502299e23a005128faee060f7e84bbd75cfb5dcc
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-30 11:22:00 +0300

    ADD: First person description

--- Cell.swift
+++ Cell.swift
@@ -11,7 +11,9 @@ struct Cell: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             TopView()
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("Do you want to download free song for ipod? If so, reading this article could save you from getting in to a lot of trouble! Downloading music to your Ipod has more than one pitfall associated with it, and this article will tell you the best way to download free song for Ipod.")
+                .lineLimit(nil)
+                .padding()
         }
     }
 }
