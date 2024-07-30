44d5c673e9915bec88c88c0eb817a5ee229fff71
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-30 08:30:37 +0300

    ADD: Horizontal stack

--- TopView.swift
+++ TopView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct TopView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("e-mail")
+            Text("likes")
+        }
     }
 }
 
