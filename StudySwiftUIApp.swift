9f11cdb9a2fe40d1353da8412e9ea0fcd9d81407
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-30 07:02:39 +0300

    Initial commit

---
+++ StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by Karina Osadchaya on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
