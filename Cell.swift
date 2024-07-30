57a1a4ca63834d019010504a01284f68a6cca3f8
Karina Osadchaya <dyach.zero@gmail.com>
2024-07-30 11:15:31 +0300

    ADD: Cell file

---
+++ Cell.swift
@@ -0,0 +1,20 @@
+//
+//  Cell.swift
+//  StudySwiftUI
+//
+//  Created by Karina Osadchaya on 7/30/24.
+//
+
+import SwiftUI
+
+struct Cell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Cell_Previews: PreviewProvider {
+    static var previews: some View {
+        Cell()
+    }
+}
