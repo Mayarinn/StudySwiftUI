b11c5b44b5d509943dd6176ebecfe3dc39a2a163
Karina Osadchaya <dyach.zero@gmail.com>
2024-08-02 08:55:19 +0300

    ADD: Detail screen

---
+++ Detail/DetailScreen.swift
@@ -0,0 +1,20 @@
+//
+//  DetailScreen.swift
+//  StudySwiftUI
+//
+//  Created by Karina Osadchaya on 8/2/24.
+//
+
+import SwiftUI
+
+struct DetailScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailScreen()
+    }
+}
