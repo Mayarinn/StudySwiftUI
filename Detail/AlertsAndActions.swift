8d35d7e1cf10abff6d8eef486d7bb0a74a3e9a60
Karina Osadchaya <dyach.zero@gmail.com>
2024-08-05 16:35:19 +0300

    ADD: Alerts and Actions file

---
+++ Detail/AlertsAndActions.swift
@@ -0,0 +1,20 @@
+//
+//  AlertsAndActions.swift
+//  StudySwiftUI
+//
+//  Created by Karina Osadchaya on 8/5/24.
+//
+
+import SwiftUI
+
+struct AlertsAndActions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlertsAndActions_Previews: PreviewProvider {
+    static var previews: some View {
+        AlertsAndActions()
+    }
+}
