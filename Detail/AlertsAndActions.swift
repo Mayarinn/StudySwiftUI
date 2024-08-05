80f88a1f0316fbde4eb1ecd7cbcf04053ed8da3c
Karina Osadchaya <dyach.zero@gmail.com>
2024-08-05 16:41:07 +0300

    UPD: Move Confirmation dialog to separate file

--- Detail/AlertsAndActions.swift
+++ Detail/AlertsAndActions.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct AlertsAndActions: View {
+    
+    @State private var isShowingConfirmation = false
+    
+    var object: ProductsResponse
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            isShowingConfirmation = true
+        } label: {
+            Label("Delete from \(object.category.rawValue)", systemImage: "trash.fill")
+        }
+        .buttonStyle(.bordered)
+        .tint(.red)
+        .controlSize(.regular)
+        
+        
+        .confirmationDialog("Delete from \(object.category.rawValue)",
+                            isPresented: $isShowingConfirmation,
+                            titleVisibility: .visible,
+                            actions: {
+            Button(role: .destructive, action: {print("Item deleted")}, label: {Text("Delete")})
+            Button(role: .destructive, action: {print("Other Item deleted")}, label: {Text("Other Item Delete")})
+                                    },
+            message: {Text("Do you want to delete from course?")
+            }
+        )
+        
     }
 }
 
 struct AlertsAndActions_Previews: PreviewProvider {
     static var previews: some View {
-        AlertsAndActions()
+        AlertsAndActions(object: materialResponse[1])
     }
 }
--- Detail/DetailScreen.swift
+++ Detail/DetailScreen.swift
@@ -9,8 +9,6 @@ import SwiftUI
 
 struct DetailScreen: View {
     
-    @State private var isShowingConfirmation = false
-    
     var object: ProductsResponse
     
     var body: some View {
@@ -24,26 +22,7 @@ struct DetailScreen: View {
                 .padding()
             Spacer()
             
-            Button {
-                isShowingConfirmation = true
-            } label: {
-                Label("Delete from \(object.category.rawValue)", systemImage: "trash.fill")
-            }
-            .buttonStyle(.bordered)
-            .tint(.red)
-            .controlSize(.regular)
-            
-            .confirmationDialog("Delete from \(object.category.rawValue)",
-                                isPresented: $isShowingConfirmation,
-                                titleVisibility: .visible,
-                                actions: {
-                Button(role: .destructive, action: {print("Item deleted")}, label: {Text("Delete")})
-                Button(role: .destructive, action: {print("Other Item deleted")}, label: {Text("Other Item Delete")})
-                                        },
-                message: {Text("Do you want to delete from course?")
-                }
-            )
-            
+            AlertsAndActions(object: object)
         }
     }
     
