e1a2bee26c6fa75f749e44ddaefb27346004eb9c
Karina Osadchaya <dyach.zero@gmail.com>
2024-08-05 12:47:29 +0300

    ADD: Teachers list shows content cells

--- FriendsList.swift
+++ FriendsList.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct FriendsList: View {
     var body: some View {
-        Text("Hello")
-            .navigationTitle("Teachers")
+        VStack {
+            ForEach(userResponse) { user in
+                Cell(user: user)
+                    .lineLimit(nil)
+            }
+        }
+        .navigationTitle("Teachers")
     }
 }
 
